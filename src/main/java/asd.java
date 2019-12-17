789093965974af21431caf03952dc582bec6ea3c
孙豪远 <yuan5230@outlook.com>
2019-12-17 21:12:19 +0800

    asd

---
+++ src/main/java/asd.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: SunHaoyuan
+ * @Date: 2019/12/17 21:11
+ * @Description:
+ */
+public class asd {
+    public static void main(String[] args) {
+        System.out.println("github");
+    }
+}
